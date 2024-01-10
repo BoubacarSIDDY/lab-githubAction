e3edf0fa2c1362cbd5d4785e981debef0faa6787
Boubacar Siddy Diallo <=>
2024-01-10 19:51:24 +0000

    first commit

---
+++ src/main/java/com/groupeisi/labgithubAction/LabGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.labgithubAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabGithubActionApplication.class, args);
+	}
+
+}
